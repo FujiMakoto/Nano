03a5a1573228eea026e726ca18d61fe8f40fd0c0
Makoto <FujiMakoto@users.noreply.github.com>
2015-04-04 11:37:29 -0400

    Remove forgotten debug print statement

--- interfaces/irc/commander.py
+++ interfaces/irc/commander.py
@@ -34,7 +34,6 @@ class IRCCommander(Commander):
         Returns:
             list, tuple, str or None: Returns replies to send to the client, or None if nothing should be returned
         """
-        print(__name__)
         # Command prefixes
         admin_prefix = "admin_command_"
         user_prefix  = "user_command_"
