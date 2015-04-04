e793391c0e210c1c253730b8e5d62c43269e39a9
Makoto <FujiMakoto@users.noreply.github.com>
2015-04-04 11:04:02 -0400

    Plugin interface support

--- src/plugins.py
+++ src/plugins.py
@@ -4,21 +4,20 @@ import pkgutil
 import logging
 from configparser import ConfigParser
 
-__author__     = "Makoto Fujikawa"
-__copyright__  = "Copyright 2015, Makoto Fujikawa"
-__version__    = "1.0.0"
-__maintainer__ = "Makoto Fujikawa"
-
 
 class PluginManager:
     """
     Universal plugin manager
     """
-    def __init__(self):
+    def __init__(self, interfaces):
         """
         Initialize a new Plugin Manager instance
+
+        Args:
+            interfaces(dict): A dictionary of interfaces to load plugins for
         """
         self.log = logging.getLogger('nano.plugin_manager')
+        self.interfaces = interfaces
         self.plugins = {}
 
         # Load our system plugins path
@@ -73,7 +72,7 @@ class PluginManager:
 
         # Load the plugin and add it to our plugins dictionary
         self.log.info('[LOAD] ' + name)
-        self.plugins[name.lower()] = Plugin(name, self.plugins_base_path, path, plugin_config)
+        self.plugins[name.lower()] = Plugin(name, self.plugins_base_path, path, plugin_config, self.interfaces)
 
     def unload_plugin(self, name):
         """
@@ -161,167 +160,165 @@ class Plugin:
     """
     Plugin handler
     """
-    # TODO: Redundant arguments
-    def __init__(self, name, plugins_base_path, plugin_path, config):
+    def __init__(self, name, base_path, path, config, interfaces):
         """
         Initialize a new Plugin instance
 
         Args:
             name(str): The module name of the plugin to load
-            plugins_base_path(str): The base directory for all system plugins
-            plugin_path(str): The directory path of the plugin being loaded
-            config(configparser.ConfigParser): The Plugin configuration
+            base_path(str): The base directory for all system plugins
+            path(str): The directory path of the plugin being loaded
+            config(configparser.ConfigParser or None): The Plugin configuration
+            interfaces(dict): A dictionary of active interfaces
         """
         # Set up the universal plugin logger
         self.log = logging.getLogger('nano.plugin')
 
         # Set our paths
-        self.plugins_base_path = plugins_base_path
-        self.plugin_path = plugin_path
+        self.base_path = base_path
+        self.path = path
 
-        # Set the plugin name, configuration and class placeholders
+        # Set the plugin name, configuration and class dictionaries
         self.name = name
         self.config = config
-        self.commands_class = None
-        self.events_class = None
-        self.cli_commands_class = None
+        self.command_classes = {}
+        self.event_classes = {}
 
         # Import the plugin
         self.log.debug('Importing plugin: ' + name)
-        self.import_path = "{plugins_dir}.{plugin_name}".format(plugins_dir=self.plugins_base_path, plugin_name=name)
-        self.module_import = importlib.import_module(self.import_path)
+        self.interfaces = interfaces if isinstance(interfaces, dict) else {}
+        self.module_imports = {}
+        self._load_imports()
 
-        # Finally, load and initialize everything
+        # Finally, load and initialize the available Command and Event classes
         self._load_plugin()
+
+    def _load_imports(self):
+        """
+        Load plugin imports on all available interfaces
+        """
+        for name, interface in self.interfaces.items():
+            import_path = "{dir}.{name}.{interface}".format(dir=self.base_path, name=self.name, interface=name)
+            try:
+                self.module_imports[name] = importlib.import_module(import_path)
+            except ImportError:
+                continue
 
     def _load_plugin(self):
         """
         Attempt to load the Commands and Events classes for the specified plugin
-
-        Args:
-            name(str): Name of the module to import
         """
         self.log.debug('Loading plugin: ' + self.name)
-        # See if we have a Commands class, and import it into our commands attribute if so
-        if hasattr(self.module_import, 'Commands'):
-            self.log.debug('Loading {module} Commands'.format(module=self.name))
-            command_class = getattr(self.module_import, 'Commands')
-            self.commands_class = command_class()
 
-        # See if we have an Events class, and import it into our events attribute if so
-        if hasattr(self.module_import, 'Events'):
-            self.log.debug('Loading {module} Events'.format(module=self.name))
-            event_class = getattr(self.module_import, 'Events')
-            self.events_class = event_class()
+        for name, module_import in self.module_imports.items():
+            # See if we have a Commands class, and import it into our commands dictionary if so
+            if hasattr(module_import, 'Commands'):
+                self.log.debug('Loading {plugin_name} {interface_name} Commands'
+                               .format(plugin_name=self.name, interface_name=name))
+                command_class = getattr(module_import, 'Commands')
+                self.command_classes[name] = command_class()
 
-        # See if we have a CLI Commands class, and import it into our cli commands attribute if so
-        if hasattr(self.module_import, 'CLICommands'):
-            self.log.debug('Loading {module} CLI Commands'.format(module=self.name))
-            command_class = getattr(self.module_import, 'Commands')
-            self.commands_class = command_class()
+            # See if we have an Events class, and import it into our events dictionary if so
+            if hasattr(module_import, 'Events'):
+                self.log.debug('Loading {plugin_name} {interface_name} Events'
+                               .format(plugin_name=self.name, interface_name=name))
+                event_class = getattr(module_import, 'Events')
+                self.event_classes[name] = event_class()
 
-    def get_irc_command(self, command_name, command_prefix='command_'):
+    def get_command(self, interface_name, command_name, command_prefix='command_'):
         """
-        Retrieve a callable IRC command method if it exists
+        Retrieve a callable command method if it exists
 
         Args:
+            interface_name(str): The name of the active interface
             command_name(str): The name of the command to retrieve
             command_prefix(str, optional): The prefix of the command method. Defaults to 'command_'
 
         Returns:
-            method or None
+            bound method or None
         """
-        self.log.debug('Requesting {prefix}{command_name} command method for {module_name}'
-                       .format(prefix=command_prefix, command_name=command_name, module_name=self.name))
+        self.log.debug('Requesting {prefix}{command_name} command method for {plugin_name}'
+                       .format(prefix=command_prefix, command_name=command_name, plugin_name=self.name))
+
+        # Make sure the requested interface has commands
+        if interface_name not in self.command_classes:
+            self.log.debug('Plugin {plugin_name} has no loaded commands for the {interface_name} interface'
+                           .format(plugin_name=self.name, interface_name=interface_name))
+            return
 
         # Return our command method if it exists
-        if command_name and hasattr(self.commands_class, command_prefix + command_name):
-            self.log.debug('Returning {prefix}{command_name} command method for {module_name}'
-                           .format(prefix=command_prefix, command_name=command_name, module_name=self.name))
-            return getattr(self.commands_class, command_prefix + command_name)
+        if command_name and hasattr(self.command_classes[interface_name], command_prefix + command_name):
+            self.log.debug('Returning {prefix}{command_name} command method for {plugin_name}'
+                           .format(prefix=command_prefix, command_name=command_name, plugin_name=self.name))
+            return getattr(self.command_classes[interface_name], command_prefix + command_name)
 
         # Otherwise return None
-        self.log.debug('{prefix}{command_name} is not a registered command for {module_name}'
-                       .format(prefix=command_prefix, command_name=command_name, module_name=self.name))
+        self.log.debug('{prefix}{command_name} is not a registered command for {plugin_name}'
+                       .format(prefix=command_prefix, command_name=command_name, plugin_name=self.name))
         return
 
-    def get_irc_event(self, event_name):
+    def get_event(self, interface_name, event_name):
         """
         Retrieve a callable IRC event method if it exists
 
         Args:
+            interface_name(str): The name of the active interface
             event_name(str): The name of the event to retrieve
 
         Returns:
-            method or None
+            bound method or None
         """
-        self.log.debug('Requesting {event_name} event method for {module_name}'
-                       .format(event_name=event_name, module_name=self.name))
+        self.log.debug('Requesting {event_name} event method for {plugin_name}'
+                       .format(event_name=event_name, plugin_name=self.name))
+
+        # Make sure the requested interface has events
+        if interface_name not in self.event_classes:
+            self.log.debug('Plugin {plugin_name} has no loaded events for the {interface_name} interface'
+                           .format(plugin_name=self.name, interface_name=interface_name))
+            return
 
         # Return our event method if it exists
-        if hasattr(self.events_class, event_name):
-            self.log.debug('Returning {event_name} event method for {module_name}'
-                           .format(event_name=event_name, module_name=self.name))
-            return getattr(self.events_class, event_name)
+        if hasattr(self.event_classes[interface_name], event_name):
+            self.log.debug('Returning {event_name} event method for {plugin_name}'
+                           .format(event_name=event_name, plugin_name=self.name))
+            return getattr(self.event_classes[interface_name], event_name)
 
         # Otherwise return None
-        self.log.debug('{event_name} is not a registered event for {module_name}'
-                       .format(event_name=event_name, module_name=self.name))
+        self.log.debug('{event_name} is not a registered event for {plugin_name}'
+                       .format(event_name=event_name, plugin_name=self.name))
         return
 
-    def get_cli_command(self, command_name, command_prefix='command_'):
-        """
-        Retrieve a callable IRC command method if it exists
-
-        Args:
-            command_name(str): The name of the command to retrieve
-
-        Returns:
-            method or None
-        """
-        self.log.debug('Requesting {prefix}{command_name} CLI command method for {module_name}'
-                       .format(prefix=command_prefix, command_name=command_name, module_name=self.name))
-
-        # Return our command method if it exists
-        if hasattr(self.cli_commands_class, command_prefix + command_name):
-            self.log.debug('Returning {prefix}{command_name} CLI command method for {module_name}'
-                           .format(prefix=command_prefix, command_name=command_name, module_name=self.name))
-            return getattr(self.cli_commands_class, command_prefix + command_name)
-
-        # Otherwise return None
-        self.log.debug('{prefix}{command_name} is not a registered CLI command for {module_name}'
-                       .format(prefix=command_prefix, command_name=command_name, module_name=self.name))
-        return
-
-    def has_irc_commands(self):
+    def has_commands(self, interface_name):
         """
         Returns True if the module has an instantiated IRC Commands class
 
+        Args:
+            interface_name(str): The name of the active interface
+
         Returns:
             bool
         """
-        self.log.debug('Checking if {module_name} has Commands'.format(module_name=self.name))
-        return self.commands_class is not None
+        self.log.debug('Checking if {plugin_name} has Commands'.format(plugin_name=self.name))
+        try:
+            return self.command_classes[interface_name] is not None
+        except KeyError:
+            return False
 
-    def has_irc_events(self):
+    def has_events(self, interface_name):
         """
         Returns True if the module has an instantiated IRC Events class
 
-        Returns:
-            bool
-        """
-        self.log.debug('Checking if {module_name} has Events'.format(module_name=self.name))
-        return self.events_class is not None
-
-    def has_cli_commands(self):
-        """
-        Returns True if the module has an instantiated CLI Commands class
+        Args:
+            interface_name(str): The name of the active interface
 
         Returns:
             bool
         """
-        self.log.debug('Checking if {module_name} has CLI Commands'.format(module_name=self.name))
-        return self.cli_commands_class is not None
+        self.log.debug('Checking if {plugin_name} has Events'.format(plugin_name=self.name))
+        try:
+            return self.event_classes[interface_name] is not None
+        except KeyError:
+            return False
 
     def __str__(self):
         """
