3545d2d14ff53d6a5157e9966a36c4a824d83525
Makoto <FujiMakoto@users.noreply.github.com>
2015-04-06 02:45:22 -0400

    Set up Commander, Postmaster and MessageParser in the Cmd class, get_help() overriden to use inspect.getdoc, printf method for response format parsing

--- src/cmd.py
+++ src/cmd.py
@@ -2,6 +2,9 @@ import sys
 import inspect
 import cmd
 import logging
+from src.utilities import MessageParser
+from interfaces.cli.commander import CLICommander
+from interfaces.cli.postmaster import Postmaster
 
 
 class NanoCmd(cmd.Cmd):
@@ -20,6 +23,11 @@ class NanoCmd(cmd.Cmd):
         # Debug logger
         self.log = logging.getLogger('nano.cmd')
 
+        # Set up our CLICommander, Postmaster and MessageParser instances
+        self.commander = CLICommander(self)
+        self.postmaster = Postmaster(self)
+        self.message_parser = MessageParser()
+
         # A bit of a crafty method of detecting whether or not we're initializing from another Cmd instance
         if len(args):
             # Enable the "back" command if we're in a sub-process (TODO: This is probably more accurately "home")
@@ -34,17 +42,65 @@ class NanoCmd(cmd.Cmd):
         """Quit the shell session"""
         sys.exit()
 
-    def precmd(self, line):
-        """
-        Command pre-processing
-
-        Args:
-            line(str): The line to pre-process
-        """
-        return line.lower()
-
     def get_names(self):
         """
         Custom method for pulling in base class attributes, including those programmatically created
         """
         return [name for name, method in inspect.getmembers(self) if name.startswith('do_')]
+
+    def do_help(self, arg):
+        'List available commands with "help" or detailed help with "help cmd".'
+        if arg:
+            # XXX check arg syntax
+            try:
+                func = getattr(self, 'help_' + arg)
+            except AttributeError:
+                try:
+                    doc=inspect.getdoc(getattr(self, 'do_' + arg))
+                    if doc:
+                        self.stdout.write("%s\n"%str(doc))
+                        return
+                except AttributeError:
+                    pass
+                self.stdout.write("%s\n"%str(self.nohelp % (arg,)))
+                return
+            func()
+        else:
+            names = self.get_names()
+            cmds_doc = []
+            cmds_undoc = []
+            help = {}
+            for name in names:
+                if name[:5] == 'help_':
+                    help[name[5:]]=1
+            names.sort()
+            # There can be duplicates if routines overridden
+            prevname = ''
+            for name in names:
+                if name[:3] == 'do_':
+                    if name == prevname:
+                        continue
+                    prevname = name
+                    cmd=name[3:]
+                    if cmd in help:
+                        cmds_doc.append(cmd)
+                        del help[cmd]
+                    elif getattr(self, name).__doc__:
+                        cmds_doc.append(cmd)
+                    else:
+                        cmds_undoc.append(cmd)
+            self.stdout.write("%s\n"%str(self.doc_leader))
+            self.print_topics(self.doc_header,   cmds_doc,   15,80)
+            self.print_topics(self.misc_header,  list(help.keys()),15,80)
+            self.print_topics(self.undoc_header, cmds_undoc, 15,80)
+
+    def printf(self, message):
+        """
+        Format a message response before printing
+
+        Args:
+            message(str): The message to format
+        """
+        if message:
+            formatted_message = self.message_parser.html_to_cli(message)
+            print(formatted_message)
