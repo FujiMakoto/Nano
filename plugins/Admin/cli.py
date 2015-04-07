872ec5d72e456b700726508b35564ea0d5263329
Makoto <FujiMakoto@users.noreply.github.com>
2015-04-07 13:10:13 -0400

    Admin CLI commands

--- plugins/Admin/cli.py
+++ plugins/Admin/cli.py
@@ -1,7 +1,9 @@
 from src.cmd import NanoCmd
 from .Network.cli import Commands as NetworkCommands
+from .Channel.cli import Commands as ChannelCommands
 
 
+# noinspection PyMethodMayBeStatic
 class Commands(NanoCmd):
     """
     Administration commands
@@ -11,3 +13,7 @@ class Commands(NanoCmd):
     def do_network(self, arg):
         """Create, delete and modify the IRC networks"""
         NetworkCommands().cmdloop()
+
+    def do_channel(self, arg):
+        """Create, delete and modify the IRC channels"""
+        ChannelCommands().cmdloop()
