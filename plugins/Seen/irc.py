41466bc259050d9eafc1e9a1a7549f5f08c3685a
Makoto <FujiMakoto@users.noreply.github.com>
2015-04-15 18:51:12 -0400

    "When did you (last/first) see yourself?" responses, make sure names are properly capitalized

--- plugins/Seen/irc.py
+++ plugins/Seen/irc.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from .plugin import Seen, NotSeenError
 from plugins.exceptions import NotEnoughArgumentsError
@@ -23,14 +24,15 @@ class Commands:
     LAST_SEEN_RESPONSES = [
         "I last saw {name} {timedelta}.",
         "I saw {name} {timedelta}.",
-        "{name}? I saw them {timedelta}!",
-        "When did I last see {name}? I guess it was around {timedelta}."
+        "{name}? I saw them about {timedelta}",
+        "I guess it was around {timedelta}."
     ]
 
     def __init__(self, plugin):
         """
         Initialize a new Seen Commands instance
         """
+        self.log = logging.getLogger('nano.plugins.seen.irc.commands')
         self.plugin = plugin
         self.seen = Seen()
 
@@ -45,17 +47,24 @@ class Commands:
         if not command.public:
             return 'If you want to know when I first saw them, ask me in a public channel!'
 
+        # Set name / logfile
         logfile = command.connection.channel_logger.logfile_path
         try:
-            name = command.args[0]
+            name = command.args[0].capitalize()
         except IndexError:
             raise NotEnoughArgumentsError
 
+        # Are we asking when did we first see ourselves?
+        if name.lower() == command.connection.network.nick.lower():
+            return "Why are you asking me when I first met myself? You're a weird person!"
+
+        # Have we seen this person?
         try:
             seen = self.seen.first(name, logfile)
         except NotSeenError:
             return random.choice(self.NOT_SEEN_RESPONSES).format(name=name)
 
+        # Return a random first seen response
         return random.choice(self.FIRST_SEEN_RESPONSES).format(name=seen.name, timedelta=seen.timedelta)
 
     def command_last(self, command):
@@ -69,15 +78,22 @@ class Commands:
         if not command.public:
             return 'If you want to know when I last saw them, ask me in a public channel!'
 
+        # Set name / logfile
         logfile = command.connection.channel_logger.logfile_path
         try:
-            name = command.args[0]
+            name = command.args[0].capitalize()
         except IndexError:
             raise NotEnoughArgumentsError
 
+        # Are we asking when did we last saw ourselves?
+        if name.lower() == command.connection.network.nick.lower():
+            return "Why are you asking me when I last saw myself? You're a weird person!"
+
+        # Have we seen this person?
         try:
             seen = self.seen.last(name, logfile)
         except NotSeenError:
             return random.choice(self.NOT_SEEN_RESPONSES).format(name=name)
 
+        # Return a random first seen response
         return random.choice(self.LAST_SEEN_RESPONSES).format(name=seen.name, timedelta=seen.timedelta)
